5dcade3581f9437a9bebd65057b58baf400630b8
usacheva_vs <viking.usach@gmail.com>
2020-09-17 18:15:07 +0700

    Exercise 7: go through all sections and check for headers

--- python-mod/exercise7.py
+++ python-mod/exercise7.py
@@ -21,8 +21,7 @@ def test_admin_menu(driver):
         menu = driver.find_element_by_id('box-apps-menu')
         menu.find_elements_by_id("app-")[i].click()
         try:
-            driver.find_elements_by_css_selector('td')[2].find_element_by_css_selector('h2')
-            break
+            driver.find_elements_by_css_selector('td')[2].find_element_by_css_selector('h1')
         except NoSuchElementException:
             print('NO HEADER')
         if len(driver.find_elements_by_class_name('docs')) > 0:
@@ -32,8 +31,7 @@ def test_admin_menu(driver):
                 submenu = driver.find_element_by_class_name('docs')
                 submenu.find_elements_by_css_selector('li')[j].click()
                 try:
-                    driver.find_elements_by_css_selector('td')[2].find_element_by_css_selector('h2')
-                    break
+                    driver.find_elements_by_css_selector('td')[2].find_element_by_css_selector('h1')
                 except NoSuchElementException:
                     print('NO HEADER')
                 j += 1
