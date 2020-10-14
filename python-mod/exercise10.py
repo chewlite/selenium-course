da769bf40cc07c3cb127c7a50d76feb303700c2b
usacheva_vs <viking.usach@gmail.com>
2020-10-14 18:53:30 +0700

    Exercise 10: all checks are done

--- python-mod/exercise10.py
+++ python-mod/exercise10.py
@@ -1,6 +1,5 @@
 import pytest
 from selenium import webdriver
-import time
 
 
 @pytest.fixture
@@ -19,20 +18,106 @@ def test_correct_product_page(driver):
     checked_products = []
     while True:
         products = driver.find_elements_by_class_name('product')
-        result = compare_names(products, checked_products, driver)
+        result = check_details(products, checked_products, driver)
         if result is None:
             break
         checked_products.append(result)
         driver.back()
 
 
-def compare_names(products, checked_products, driver):
+def check_details(products, checked_products, driver):
     for p in products:
+
         card = p.find_element_by_css_selector('a')
         name_main = card.get_property('title')
+
         if name_main not in checked_products:
+
+            price_main = ''
+            discount_price_main = ''
+            simple_price_main = card.find_elements_by_class_name('price')  # единственная цена в списке
+            regular_price_main = card.find_elements_by_class_name('regular-price')  # обычная цена в списке
+            campaign_price_main = card.find_elements_by_class_name('campaign-price')  # акционная цена в списке
+
+            if len(simple_price_main) > 0:
+                price_main = simple_price_main[0].text
+
+            if len(regular_price_main) > 0 and len(campaign_price_main) > 0:
+
+                price_main = regular_price_main[0].text
+
+                # проверка: цвет обычной цены в списке - серый
+                pm_color = regular_price_main[0].value_of_css_property("color")
+                pm_rgb = pm_color[5:len(pm_color)-1].split(', ')
+                assert pm_rgb[0] == pm_rgb[1] == pm_rgb[2]
+
+                # проверка: обычная цена в списке - зачеркнутая
+                pm_style = regular_price_main[0].value_of_css_property("text-decoration-line")
+                assert pm_style == 'line-through'
+
+                # проверка: цвет акционной цены в списке - красный
+                discount_price_main = campaign_price_main[0].text
+                dpm_color = campaign_price_main[0].value_of_css_property("color")
+                dpm_rgb = dpm_color[5:len(dpm_color)-1].split(', ')
+                assert dpm_rgb[1] == dpm_rgb[2] == '0'
+
+                # проверка: акционная цена в списке - жирная
+                dpm_style = campaign_price_main[0].value_of_css_property("font-weight")
+                assert dpm_style == '700'
+
+                # проверка: акционная цена в списке больше обычной цены
+                pm_size = float(regular_price_main[0].value_of_css_property("font-size").split('px')[0])
+                dpm_size = float(campaign_price_main[0].value_of_css_property("font-size").split('px')[0])
+                assert pm_size < dpm_size
+
             card.click()
+
+            # проверка: имена продукта в списке и в деталях совпадают
             name_details = driver.find_element_by_css_selector('h1').text
             assert name_details == name_main
-            return name_details
+
+            price_wrapper = driver.find_element_by_class_name('price-wrapper')
+            simple_price_details = price_wrapper.find_elements_by_class_name('price')  # единственная цена в деталях
+            regular_price_details = price_wrapper.find_elements_by_class_name('regular-price')  # обычная цена в деталях
+            campaign_price_details = price_wrapper.find_elements_by_class_name('campaign-price')  # акц. цена в деталях
+
+            # проверка: единственная цена в списке и в деталях совпадают
+            if len(simple_price_details) > 0:
+                price_details = simple_price_details[0].text
+                assert price_details == price_main
+
+            if len(regular_price_details) > 0 and len(campaign_price_details) > 0:
+
+                # проверка: обычная цена в списке и в деталях совпадают
+                price_details = regular_price_details[0].text
+                assert price_details == price_main
+
+                # проверка: обычная цена в деталях - серая
+                pd_color = regular_price_details[0].value_of_css_property("color")
+                rgb = pd_color[5:len(pd_color)-1].split(', ')
+                assert rgb[0] == rgb[1] == rgb[2]
+
+                # проверка: обычная цена в деталях - зачеркнутая
+                pd_style = regular_price_details[0].value_of_css_property("text-decoration-line")
+                assert pd_style == 'line-through'
+
+                # проверка: обычная цена в деталях - серая
+                discount_price_details = campaign_price_details[0].text
+                assert discount_price_details == discount_price_main
+
+                # проверка: акционная цена в деталях - красная
+                dpd_color = campaign_price_details[0].value_of_css_property("color")
+                rgb = dpd_color[5:len(dpd_color)-1].split(', ')
+                assert rgb[1] == rgb[2] == '0'
+
+                # проверка: акционная цена в деталях - жирная
+                dpd_style = campaign_price_details[0].value_of_css_property("font-weight")
+                assert dpd_style == '700'
+
+                # проверка: акционная цена в деталях больше обычной цены
+                rpd_size = float(regular_price_details[0].value_of_css_property("font-size").split('px')[0])
+                dpd_size = float(campaign_price_details[0].value_of_css_property("font-size").split('px')[0])
+                assert rpd_size < dpd_size
+
+            return name_main
     return None
