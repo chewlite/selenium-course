e5a9daea3e7e2883cf9fcabb38fe9b70923ed111
usacheva_vs <viking.usach@gmail.com>
2020-11-12 21:13:19 +0700

    Exercise 17: logs (small fix)

--- python-mod/exercise17.py
+++ python-mod/exercise17.py
@@ -12,7 +12,7 @@ def driver(request):
     return wd
 
 
-def test_windows(driver):
+def test_logs(driver):
     driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1")
     driver.find_element_by_name('username').send_keys('admin')
     driver.find_element_by_name('password').send_keys('admin')
